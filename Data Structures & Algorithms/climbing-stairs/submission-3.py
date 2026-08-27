b284c87b0bc2354457aa345d8ac93e9d65004d4d
rohilhverma <rohilhverma@gmail.com>
2026-08-26 17:13:01 -0700

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dct = {2:2,1:1}
+        def help(n, dct):
+            if n in dct:
+                return dct[n]
+            else:
+                dct[n] = help(n-1, dct) + help(n-2, dct)
+            return dct[n]
+        return help(n, dct)
